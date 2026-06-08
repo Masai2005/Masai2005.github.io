be27e7847ef0caace39e7b2bd300be63beaa500b
Blessed Rafael Saigarai <saigaraithei@gmail.com>
2026-06-08 11:01:46 +0300

    Fix: changes to match github workflows

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://masai2005.github.io',
-  base: '/portfolio-master',
   vite: {
     plugins: [tailwindcss()]
   }
